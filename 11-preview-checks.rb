526a50b09835436908561aa4e3d30da75f758d57
Jiri Tobolka <jiri.tobolka@bizztreat.com>
2015-09-03 15:34:46 +0200

    FEATURE: test preview for metrics and reports both are correctly tagged and used

---
+++ 11-preview-checks.rb
@@ -0,0 +1,69 @@
+require 'date'
+require 'gooddata'
+require 'csv'
+require 'optparse'
+require 'yaml'
+
+options = {}
+OptionParser.new do |opts|
+    
+    opts.on('-u', '--username USER', 'Username') { |v| options[:username] = v }
+    opts.on('-p', '--password PASS', 'Password') { |v| options[:password] = v }
+    opts.on('-s', '--startproject NAME', 'Start Project') { |v| options[:start] = v }
+    opts.on('-d', '--develproject NAME', 'Development Project') { |v| options[:devel] = v }
+    
+end.parse!
+
+#username = ''
+#password = ''
+username = options[:username]
+password = options[:password]
+
+# change the tags to check here
+tag = 'preview'
+
+puts 'Connecting to GoodData...'
+puts 'Testing Report results between Start and Devel projects.'
+
+GoodData.with_connection(username, password) do |client|
+    
+    
+    start = client.projects(options[:start])
+    #devel = client.projects(options[:devel])
+    #start = client.projects('x1c6gsmxhr84usnhww03s6ecx3625279')
+    #devel = client.projects('t3m4hv0v5vrysctjqax88t2q2346t6vd')
+    
+    # We assume that reports have unique name inside a project
+    
+        metrics = start.metrics.select {|m| m.tag_set.include?(tag)}.sort_by(&:title)
+        
+        metrics.each do |met|
+
+            puts met.title
+            
+            reports = met.usedby
+            
+            puts '--- --- --- --- '
+            puts 'Reports that contains PREVIEW metrics and are not tagged PREVIEW:'
+            #puts reports
+            
+            reports.select  {|report| report["category"] == 'report'}.each { |r|
+                    # get only report objects and extract tags
+                    obj = GoodData::get(r["link"])
+                    
+                    # check whether reports include preview tag
+                    # puts obj['report']['meta']['title']
+                    if obj['report']['meta']['tags'].include? "preview" then else
+                        puts "https://secure.gooddata.com#{obj['report']['meta']['uri']}"
+                    end
+                }
+            
+            
+            puts '--- --- --- --- '
+          
+    end
+    
+end
+
+puts 'Disconnecting...'
+GoodData.disconnect
