bbbf8005a33cdf81172089bf7addf7c1f54851f4
Jiri Tobolka <jiri.tobolka@bizztreat.com>
2016-01-27 11:14:06 +0100

    #5 standardize output

--- testing/5-test-aggregations.rb
+++ testing/5-test-aggregations.rb
@@ -23,16 +23,21 @@ start = options[:start]
 devel = options[:devel]
 server = options[:server]
 
+# variables for standard output
+counter_ok = 0
+counter_err = 0
+err_array = []
+result = []
+
+# turn off logging for clear output
+GoodData.logging_off
+
 # if whitelabel is not specified set to default domain
 if server.to_s.empty? then server = 'https://secure.gooddata.com' end
 
 # specify aggregations to check
 aggregations = [:sum,:avg,:median,:min,:max]
 #aggregations = [:sum]
-
-puts 'Connecting to GoodData...'
-puts 'Testing fact aggregations between Start and Devel projects.'
-puts 'It might took a while...please wait'
 
 # connect to GoodData
 GoodData.with_connection(login: username, password: password, server: server) do |client|
@@ -42,7 +47,7 @@ GoodData.with_connection(login: username, password: password, server: server) do
         $start_results = {}
             project.facts.each do |fact|
                 aggregations.each do |aggr|
-                    metric = fact.create_metric(:title => "VALIDATION - #{aggr} of [#{fact.identifier}]", :type => aggr)
+                    metric = fact.create_metric(:title => "#{fact.uri}", :type => aggr)
                     res = metric.execute
                     $start_results[metric.title] = res
                 end
@@ -57,7 +62,7 @@ GoodData.with_connection(login: username, password: password, server: server) do
         project.facts.each do |fact|
             
             aggregations.each do |aggr|
-                metric = fact.create_metric(:title => "VALIDATION - #{aggr} of [#{fact.identifier}]", :type => aggr)
+                metric = fact.create_metric(:title => "#{fact.uri}", :type => aggr)
                 res = metric.execute
                 $devel_fact_results[metric.title] = res
             end
@@ -68,9 +73,32 @@ GoodData.with_connection(login: username, password: password, server: server) do
     
     # compare results between projects
     $devel_fact_results.each do |key, value|
-        if $start_results[key] != $devel_fact_results[key] then puts key.to_s + ' - NOT MATCH' else puts key.to_s + ' - CORRECT' end
+        if $start_results[key] != $devel_fact_results[key]
+            then
+            # count errors and prepare details to the array
+            counter_err += 1
+            error_details = {
+                :type => "ERROR",
+                :url => 'https://secure.gooddata.com/#s=/gdc/projects' + devel +'|objectPage|' + key.to_s,
+                :api => 'secure.gooddata.com' + key.to_s,
+                :message => "Aggregation is different"
+            }
+            
+            # save detail to the array
+            err_array.push(JSON.generate(error_details))
+            
+            # count OK objects
+            else counter_ok += 1
     end
+    
+  end
+    
+    
+    # prepare part of the results
+    result.push({:section => 'Compare aggregations between start and devel project', :OK => counter_ok, :ERROR => counter_err, :output => err_array})
+    
+    puts result.to_json
+    
 end
 
-puts 'Disconnecting...'
 GoodData.disconnect
