0c4e3781b64d1598798d0022627f2a7e4404e22a
Jiri Tobolka <jiri.tobolka@bizztreat.com>
2016-02-21 17:08:12 +0100

    standard output 16 & 17

--- testing/16-simple-metric-result-test.rb
+++ testing/16-simple-metric-result-test.rb
@@ -1,3 +1,4 @@
+#Testing Report results between Start and Devel projects.
 require 'date'
 require 'gooddata'
 require 'csv'
@@ -10,44 +11,59 @@ OptionParser.new do |opts|
     
     opts.on('-u', '--username USER', 'Username') { |v| options[:username] = v }
     opts.on('-p', '--password PASS', 'Password') { |v| options[:password] = v }
-    opts.on('-s', '--startproject NAME', 'Start Project') { |v| options[:start] = v }
-    opts.on('-d', '--develproject NAME', 'Development Project') { |v| options[:devel] = v }
+    opts.on('-d', '--develproject NAME', 'Devel Project') { |v| options[:devel] = v }
     opts.on('-h', '--hostname NAME', 'Hostname') { |v| options[:server] = v }
-
+    # change the tags to check here. Use this format only!:['tag1','tag2'] for example:['qa','test'] 
+    opts.on('-t', '--tags TAGS', 'Tags') { |v| options[:tags] = v }
 end.parse!
 
 # get credentials from input parameters
 username = options[:username]
 password = options[:password]
+devel = options[:devel]
 server = options[:server]
+tag = tags[:server]
+
+# variables for script results
+result_array = []
+$result = []
 
 # if whitelabel is not specified set to default domain
 if server.to_s.empty? then server = 'https://secure.gooddata.com' end
 
-# change the tags to check here
-tag = ['qa','test']
-
-puts 'Connecting to GoodData...'
-puts 'Testing Report results between Start and Devel projects.'
+# turn off GoodData logging
+GoodData.logging_off
 
 # connect to GoodData
 GoodData.with_connection(login: username, password: password, server: server) do |client|
 
        # get the project context using Project ID from user input
-       devel = client.projects(options[:devel])
+       devel_project = client.projects(devel)
        
        # for each tag
        tag.each do |tag|
        
        # get metrics that include specific tag
-       metrics = devel.metrics.select {|r| r.tag_set.include?(tag)}
+       metrics = devel_project.metrics.select {|r| r.tag_set.include?(tag)}
        
        # print metric result
-       metrics.each { |m| puts m.title + ' : ' + m.execute.to_s }
+       metrics.each { |m|  
        
+       result_array.push(error_details = {
+                               :type => "INFO",
+                               :url => server + '/#s=/gdc/projects/' + devel + '|objectPage|' + m.uri ,
+                               :api => server + m.uri,
+                               :message => 'Results of the metric ('+ m.title + ') is: ' +  m.execute.to_s
+                               })
+      }                         
+           
+    
        end
+    #save errors in the result variable
+    $result.push({:section => 'Metric results between Start and Devel projects.', :OK => 0, :ERROR => 0, :output => result_array})
+  end
 
-end
+#print out the result
+puts $result.to_json
 
-puts 'Disconnecting...'
 GoodData.disconnect
--- testing/17-compare-metric-between-projects.rb
+++ testing/17-compare-metric-between-projects.rb
@@ -1,3 +1,4 @@
+#Testing Metric results between Start and Devel projects.
 require 'date'
 require 'gooddata'
 require 'csv'
@@ -13,6 +14,8 @@ OptionParser.new do |opts|
     opts.on('-s', '--startproject NAME', 'Start Project') { |v| options[:start] = v }
     opts.on('-d', '--develproject NAME', 'Development Project') { |v| options[:devel] = v }
     opts.on('-h', '--hostname NAME', 'Hostname') { |v| options[:server] = v }
+    # change the tags to check here. Use this format only!:['tag1','tag2'] for example:['qa','test'] 
+    opts.on('-t', '--tags TAGS', 'Tags') { |v| options[:tags] = v }
 
 end.parse!
 
@@ -20,31 +23,34 @@ end.parse!
 username = options[:username]
 password = options[:password]
 server = options[:server]
+start = client.projects(options[:start])
+devel = client.projects(options[:devel])
+tag = client.projects(options[:tags])
+
+# variables for script results
+result_array = []
+$result = []
 
 # if whitelabel is not specified set to default domain
 if server.to_s.empty? then server = 'https://secure.gooddata.com' end
 
-# change the tags to check here
-tag = ['qa','test']
-
-puts 'Connecting to GoodData...'
-puts 'Testing Metric results between Start and Devel projects.'
+# turn off GoodData logging
+GoodData.logging_off
 
 # connect to gooddata project
 GoodData.with_connection(login: username, password: password, server: server) do |client|
     
     
-       start = client.projects(options[:start])
-       devel = client.projects(options[:devel])
-       
+       start_project = client.projects(start)
+       devel_project = client.projects(devel)
        # for each tag
        tag.each do |tag|
        
        # get metric from start project
-       orig_metrics = start.metrics.select {|r| r.tag_set.include?(tag)}.sort_by(&:uri)
+       orig_metrics = start_project.metrics.select {|r| r.tag_set.include?(tag)}.sort_by(&:uri)
        
        # get metrics from devel project
-       new_metrics = devel.metrics.select {|r| r.tag_set.include?(tag)}.sort_by(&:uri)
+       new_metrics = devel_project.metrics.select {|r| r.tag_set.include?(tag)}.sort_by(&:uri)
 
        # compare results
        results = orig_metrics.zip(new_metrics).pmap do |metrics|
@@ -54,13 +60,27 @@ GoodData.with_connection(login: username, password: password, server: server) do
        
        # print results for both metrics from start and devel
        results.map do |res|
-           orig_result, new_result, new_metrics = res
-           puts "#{new_metrics.title}, #{orig_result == new_result}"
+           orig_result, new_result, new_metrics = res           
+           
+         result_array.push(error_details = {
+                               :type => "INFO",
+                               :url => server + '/#s=/gdc/projects/' + devel + '|objectPage|' + new_metrics.uri ,
+                               :api => server + new_metrics.uri,
+                               :message => 'Results of the metric ('+ new_metrics.title + ') are in both projects equal.'
+                               })
+           
+           
        end
        
        end
 
+    #save errors in the result variable
+    $result.push({:section => 'Metric results between Start and Devel projects.', :OK => 0, :ERROR => 0, :output => result_array})
+    
 end
 
-puts 'Disconnecting...'
+#print out the result
+puts $result.to_json
+
+
 GoodData.disconnect
