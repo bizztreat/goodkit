d86ccb39d830ea82320add07887be52e5152abbc
Michal Zima <zima.majkl@gmail.com>
2016-04-11 16:58:21 +0800

    fix issues and changes in behaviour
    Celková změna chování podle požadavků

--- testing/fix-time-labels.rb
+++ testing/fix-time-labels.rb
@@ -1,11 +1,13 @@
-#Fix time attributes - move to "Date and Time" folder
-# and delete characters from name of the attribute when the parameter "remove" is set to value "true"
+#Fix time attributes - Moving attributes to own folders and renaming attributes
+# FOLDERS parametr (true/false) - delete empty folders or not
+# MAIN parametr (string) - The Data set name of the main Date and Time dimension
 require 'date'
 require 'gooddata'
 require 'csv'
 require 'optparse'
 require 'yaml'
 
+# ---------------------METHODS---------------------
 # Method for creating a folder
 def create_folder(project, title, description = "")
   res = project.client.post(project.md['obj'], {"dimension":
@@ -14,17 +16,62 @@ def create_folder(project, title, description = "")
   project.objects(res['uri'])
 end
 
+# Method Create a folder name from Dataset name
+def create_folder_name(title, main_DD)
+if title == main_DD then
+name = "Date and Time"
+else
+name = title.sub('(', '')
+name = name.sub(')', '')
+name = name.sub('_ts', '')
+name = name.sub('_date', '')
+name = name.sub('_', '')
+name = name.sub('Date', 'Date -')
+name = name.split(/(?=[A-Z])/).join(' ').upcase
+name = name.split.map(&:capitalize).join(' ')
+name = name.sub('D D D', 'DDD')
+end
+end
 
+# Method Create a folder name from Dataset name
+def create_attribute_name(attr_title, dataset_title, main_DD, attr_identifier)
+  first_name = ""
+  case attr_identifier[attr_identifier.index('.')+1..-1]
+  when 'date' then first_name = 'Date'
+  when 'day.in.month' then first_name = 'Day of Month'
+  when 'day.in.quarter' then first_name = 'Day of Quarter'
+  when 'day.in.euweek' then first_name = 'Day of Week (Mon-Sun)'
+  when 'day.in.week' then first_name = 'Day of Week (Sun-Sat)'
+  when 'day.in.year' then first_name = 'Day of Year'
+  when 'month' then first_name = 'Month/Year'
+  when 'month.in.quarter' then first_name = 'Month of Quarter'
+  when 'month.in.year' then first_name = 'Month'
+  when 'quarter' then first_name = 'Quarter'
+  when 'quarter.in.year' then first_name = 'Quarter/Year'
+  when 'euweek' then first_name = 'Week (Mon-Sun)'
+  when 'euweek.in.quarter' then first_name = 'Week (Mon-Sun) of Qtr'
+  when 'euweek.in.year' then first_name = 'Week (Mon-Sun)/Year'
+  when 'week' then first_name = 'Week (Sun-Sat)'
+  when 'week.in.quarter' then first_name = 'Week (Sun-Sat) of Qtr'
+  when 'week.in.year' then first_name = 'Week (Sun-Sat)/Year'
+  when 'year' then first_name = 'Year'
+  end
+  if dataset_title == main_DD then
+  name = first_name
+  else
+  name = first_name + ' ' + create_folder_name(dataset_title, main_DD).sub('Date ', '')
+  end
+end
+
+# ----------------------PARAMETERS---------------------
 options = {}
 OptionParser.new do |opts|
-
   opts.on('-u', '--username USER', 'Username') { |v| options[:username] = v }
   opts.on('-p', '--password PASS', 'Password') { |v| options[:password] = v }
   opts.on('-d', '--develproject NAME', 'Development Project') { |v| options[:devel] = v }
   opts.on('-h', '--hostname NAME', 'Hostname') { |v| options[:server] = v }
-  opts.on('-r', '--hostname REMOVE', 'Remove characters') { |v| options[:remove] = v }
-  opts.on('-c', '--hostname CHARS', 'Characters to remove') { |v| options[:chars] = v }
-
+  opts.on('-h', '--folders FOLDERS', 'Folders') { |v| options[:folders] = v } #  true/false delete empty folders
+  opts.on('-h', '--main MAIN', 'Main') { |v| options[:main] = v }  # string name of main Date Time dataset
 end.parse!
 
 # get all parameters - username, password and project id
@@ -32,12 +79,11 @@ username = options[:username]
 password = options[:password]
 devel = options[:devel]
 server = options[:server]
-delete_chars = options[:remove]
-chars_to_delete = options[:chars]
+folders_delete = options[:folders]
+main = options[:main]
 
 # if whitelabel is not specified set to default domain
 if server.to_s.empty? then server = 'https://secure.gooddata.com' end
-
 
 # variables for standard output
 counter_ok = 0
@@ -47,92 +93,69 @@ counter_names_err = 0
 result_array = []
 result_names_array = []
 $result = []
-folder_done = false
 
 # turn off logging for clear output or ---> GoodData.logging_http_on
 GoodData.logging_off
-#GoodData.logging_http_on
 
 # connect to gooddata
 GoodData.with_connection(login: username, password: password, server: server) do |client|
 
 # connect to project
   GoodData.with_project(devel) do |project|
-  # -----------ATTRIBUTES----------------
+
   # Let's cache all the folders containing attributes to speed things up
    folder_uris = project.attributes.map { |a| a.content['dimension'] }.uniq.compact
-
    folder_cache = folder_uris.reduce({}) { |a, e| a[e] = project.objects(e); a }
-  # Let's cache all the folders in project to check if "Date and Time" folder already exist and get it´s uri later
+  # Let's cache all the folders in project to check if our folder already exists and to get uri later
    all_folders={}
    client.get("#{project.md['query']}/dimensions")['query']['entries'].map do |i|
           all_folders.[]=(i['title'],i['link'])
       end
    all_folders.values.uniq
 
-  # Let's start to check attributes
-    project.attributes.each do |a|
+ # Let's start with DATASETS
+   project.datasets.each do |d|
+  # condition if it's DD
+   if ['.dt'].any? { |word| d.identifier.include?(word) } then
+  # Let's start with ATTRIBUTES
+      d.attributes.each do |a|
 
-     if a.identifier.include? "attr.time"
-     then
-     #change replace the string in name of attribute
-        if delete_chars then
-          if a.title.include? chars_to_delete
-                then
-                name = a.title
-                name.gsub!(chars_to_delete,'')
-                result_names_array.push(error_details = {
-                                     :type => "ERROR",
-                                     :url => server + '#s=/gdc/projects/' + devel + '|objectPage|' + a.uri  ,
-                                     :api => server + a.uri,
-                                     :message => 'The new name of the attribute is (' + name + ').'
-                                     })
-                  # count objects
-                counter_names_err += 1
+        a.title = create_attribute_name(a.title, d.title, main_DD, a.identifier)
+        a.title
+        a.save
 
-                a.meta['title'] = name
-                a.save
-                else
-                counter_names_ok += 1
-              end
-          else
-          end
+      # IF checks if the folder exists and if the name is same and one more check if the folder is not deleted from GD GUI (deprecated)
+      if folder_cache.key?(a.content['dimension']) && folder_cache[a.content['dimension']].title  == create_folder_name(d.title, main_DD) && !folder_cache[a.content['dimension']].deprecated
 
-      folder_uri = a.content['dimension']
-      # Pull folder from cache and compare the titles
-      # IF checks if the folder exists and if the name is same and one more check if the folder is not deleted form GD GUI (deprecated)
-      if folder_cache.key?(folder_uri) && folder_cache[folder_uri].title  == "Date and Time" && !folder_cache[folder_uri].deprecated
-
-         #push info detail to result array
+         #push info detail to result array as INFO
           result_array.push(error_details = {
                                :type => "INFO",
                                :url => server + '#s=/gdc/projects/' + devel + '|objectPage|' + a.uri  ,
                                :api => server + a.uri,
-                               :message => 'The attribute (' + a.title + ') is already in folder "Date and Time".'
+                               :message => 'The attribute (' + a.title + ') is already in folder "' + create_folder_name(d.title, main_DD) + '".'
                                })
             # count objects
             counter_ok += 1
-
       else
-          #push info detail to result array
+          #push info detail to result array as ERROR
           result_array.push(error_details = {
                                :type => "ERROR",
                                :url => server + '#s=/gdc/projects/' + devel + '|objectPage|' + a.uri  ,
                                :api => server + a.uri,
-                               :message => 'The attribute (' + a.title + ') has been moved to "Date and Time" folder.'
+                               :message => 'The attribute (' + a.title + ') has been moved to  folder "' + create_folder_name(d.title, main_DD) + '".'
                                })
             # count objects
         counter_err += 1
-
-        if all_folders.key?("Date and Time")
+# ACTION -- do the changes
+        if all_folders.key?(create_folder_name(d.title, main_DD))
           # "we have it so we just need to assign it"
-      a.content['dimension'] = all_folders["Date and Time"]
+      a.content['dimension'] = all_folders[create_folder_name(d.title, main_DD)]
       a.save
         else
           # we have to create it first
-          folder = create_folder(project, "Date and Time")
+          folder = create_folder(project, create_folder_name(d.title, main_DD))
           folder_cache[folder.uri] = folder
-              #refresh folders
+              #refresh folder's list
               client.get("#{project.md['query']}/dimensions")['query']['entries'].map do |i|
               all_folders.[]=(i['title'],i['link'])
               end
@@ -145,19 +168,43 @@ GoodData.with_connection(login: username, password: password, server: server) do
       end
     end
     end
-    #save info about dataset into the result variable
-    if delete_chars then
-    $result.push({:section => 'The Time attributes titles changes.', :OK => counter_names_ok, :ERROR => counter_names_err, :output => result_names_array})
-    else end
+  end
+
+    #save info into the result variable
     $result.push({:section => 'The Time attributes folders changes.', :OK => counter_ok, :ERROR => counter_err, :output => result_array})
 
     result_array = []
     counter_ok = 0
     counter_err = 0
+
+
+    #-----------DELETE EMPTY FOLDERS----------------
+    if folders_delete then
+       all_folders={}
+       client.get("#{project.md['query']}/dimensions")['query']['entries'].map do |i|
+              all_folders.[]=(i['title'],i['link'])
+          end
+
+      all_folders.each { |x, e|  if project.objects(e).content.to_s == '{"attributes"=>[]}' then
+      object = project.objects(e)
+      object.deprecated = 1
+      object.save
+      result_array.push(error_details = {
+                           :type => "INFO",
+                           :url => server + '#s=/gdc/projects/' + devel + '|objectPage|' + object.uri  ,
+                           :api => server + object.uri,
+                           :message => 'The folder (' + object.title + ') has been deleted.'
+                           })
+        # count objects
+        counter_ok += 1
+
+
+     end }
+     $result.push({:section => 'The deleted folders.', :OK => counter_ok, :ERROR => counter_err, :output => result_array})
   end
 
 end
-
+end
 
 #print out the result
 puts $result.to_json
