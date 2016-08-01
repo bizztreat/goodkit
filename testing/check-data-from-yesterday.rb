8932f69cc0cd56406a442e7210e64c67893a3b3e
JaroslavRamba <jaroslav@ramba.cz>
2016-08-01 16:18:13 +0200

    refactoring

--- testing/check-data-from-yesterday.rb
+++ testing/check-data-from-yesterday.rb
@@ -1,9 +1,3 @@
-# This script checks if there are some data from yesterday
-# You need to use two extra variables ATTRIBUTE -a and TIME -t
-# 1. The ATTRIBUTE must to be filled by uri of some attribute like an ID (Orders) in this format: /gdc/md/projectspid/obj/1664
-# 2. The TIME -t must to be filled by with attribute from time dimension.
-# For yestreday it's 'Date (your time dimension)', for previous month it's 'Month/Year (your time dimension)'
-# again use the uri in this format: /gdc/md/projectspid/obj/1663
 require 'date'
 require 'gooddata'
 require 'csv'
@@ -12,7 +6,7 @@ require 'yaml'
 require 'rubygems'
 require 'json'
 
-# prepare all parameters options
+# define options for script configuration
 options = {}
 OptionParser.new do |opts|
 
@@ -20,12 +14,12 @@ OptionParser.new do |opts|
   opts.on('-p', '--password PASS', 'Password') { |v| options[:password] = v }
   opts.on('-d', '--development_project ID', 'Development Project') { |v| options[:development_project] = v }
   opts.on('-h', '--hostname NAME', 'Hostname') { |v| options[:server] = v }
-  opts.on('-a', '--attribute ATTRIBUTE', 'Attribute') { |v| options[:attribute] = v }
-  opts.on('-t', '--time TIME', 'Time') { |v| options[:time] = v }
+  opts.on('-a', '--attribute ATTRIBUTE', 'Attribute') { |v| options[:attribute] = v } # uri of some attribute like an ID (Orders) in this format: /gdc/md/projectspid/obj/1664
+  opts.on('-t', '--time TIME', 'Time') { |v| options[:time] = v } # must to be filled by with attribute from time dimension 'Month/Year (your time dimension)'
 
 end.parse!
 
-# get credentials from user parameters
+# get credentials and others from input parameters
 username = options[:username]
 password = options[:password]
 development_project = options[:development_project]
@@ -33,52 +27,56 @@ server = options[:server].to_s.empty? ? 'https://secure.gooddata.com' : options[
 attribute = options[:attribute]
 time = options[:time]
 
-counter_ok = 0
-counter_err = 0
-result_array = []
+counter_info = 0
+counter_error = 0
+output = []
 $result = []
 
 # turn off logging for clear output
 GoodData.logging_off
-GoodData.with_connection(login: username, password: password, server: server) do |client|
-  # connect to project
-  GoodData.with_project(development_project) do |project|
-    metric = project.add_measure 'SELECT COUNT(['+ attribute +']) WHERE ['+ time +'] = PREVIOUS', title: 'Test the data'
-    metric.save
-    begin
-      if metric.execute.to_s == ''
-        result_array.push(error_details = {
-            :type => 'ERROR',
-            :url => server + '/#s=/gdc/projects/' + development_project + '|objectPage|' + metric.uri,
-            :api => server + metric.uri,
-            :title => project.title,
-            :description => 'There are no data from yesterday.'
-        })
-        counter_err += 1
-      else
-        result_array.push(error_details = {
-            :type => 'INFO',
-            :url => server + '/#s=/gdc/projects/' + development_project + '|objectPage|' + metric.uri,
-            :api => server + metric.uri,
-            :title => project.title,
-            :description => 'There are some data from yesterday.'
-        })
-        counter_ok += 1
-      end
-    rescue
-      result_array.push(error_details = {
-          :type => 'ERROR',
-          :url => server + '/#s=/gdc/projects/' + development_project + '|objectPage|' + metric.uri,
-          :api => server + metric.uri,
-          :title => project.title,
-          :description => 'There is problem with the metric. It is uncomputable.'
-      })
-      counter_err += 1
-    end
-    metric.delete
-    $result.push({:section => 'The result if there are some data from yesterday.', :OK => counter_ok, :ERROR => counter_err, :output => result_array})
 
+# connect to GoodData
+client = GoodData.connect(login: username, password: password, server: server)
+
+# connect to development GoodData project
+development_project = client.projects(development_project)
+
+metric = project.add_measure 'SELECT COUNT(['+ attribute +']) WHERE ['+ time +'] = PREVIOUS', title: 'Test the data'
+metric.save
+
+begin
+  if metric.execute.to_s == ''
+    output.push(error_details = {
+        :type => 'ERROR',
+        :url => server + '/#s=' + development_project.uri + '|objectPage|' + metric.uri,
+        :api => server + metric.uri,
+        :title => project.title,
+        :description => 'There are no data from yesterday.'
+    })
+    counter_error += 1
+  else
+    output.push(error_details = {
+        :type => 'INFO',
+        :url => server + '/#s=' + development_project.uri + '|objectPage|' + metric.uri,
+        :api => server + metric.uri,
+        :title => project.title,
+        :description => 'There are some data from yesterday.'
+    })
+    counter_info += 1
   end
-  puts $result.to_json
+rescue
+  output.push(error_details = {
+      :type => 'ERROR',
+      :url => server + '/#s=' + development_project.uri + '|objectPage|' + metric.uri,
+      :api => server + metric.uri,
+      :title => project.title,
+      :description => 'There is problem with the metric. It is uncomputable.'
+  })
+  counter_error += 1
 end
-GoodData.disconnect
+
+metric.delete
+$result.push({:section => 'The result if there are some data from yesterday.', :OK => 0, :INFO => counter_info, :ERROR => counter_error, :output => output})
+puts $result.to_json
+
+client.disconnect
