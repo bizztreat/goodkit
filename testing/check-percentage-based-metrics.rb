b6b056e231e79370aac56a6413711ff4f7b56392
JaroslavRamba <jaroslav@ramba.cz>
2016-09-05 17:13:52 +0200

    refactoring

--- testing/check-percentage-based-metrics.rb
+++ testing/check-percentage-based-metrics.rb
@@ -1,4 +1,3 @@
-# The script checks if the percentage based metric is computable first and then if the metric's result is in the right range.
 require 'gooddata'
 require 'optparse'
 
@@ -9,9 +8,9 @@ OptionParser.new do |opts|
   opts.on('-p', '--password PASS', 'Password') { |v| options[:password] = v }
   opts.on('-d', '--development_project ID', 'Development Project') { |v| options[:development_project] = v }
   opts.on('-h', '--hostname NAME', 'Hostname') { |v| options[:server] = v }
+  opts.on('-t', '--title_illegal_substrings TITLE', 'Title illegal substrings') { |v| options[:title] = v }
   opts.on('-i', '--include INCLUDE', 'Tag included') { |v| options[:tags_included] = v }
   opts.on('-e', '--exclude EXCLUDE', 'Tag excluded') { |v| options[:tags_excluded] = v }
-  opts.on('-t', '--title TITLE', 'Title containes') { |v| options[:title] = v }
 
 end.parse!
 
@@ -22,8 +21,7 @@ development_project = options[:development_project]
 server = options[:server].to_s.empty? ? 'https://secure.gooddata.com' : options[:server]
 tags_included = options[:tags_included].to_s.split(',')
 tags_excluded = options[:tags_excluded].to_s.split(',')
-special_tags = options[:title].to_s.empty? ? "%,Percentage,Ratio".to_s.split(',') : options[:title].to_s.split(',')
-
+title_illegal_substrings = options[:title_illegal_substrings].to_s.empty? ? '%,Percentage,Ratio'.to_s.split(',') : options[:title_illegal_substrings].to_s.split(',')
 
 # variables for script results
 output = []
@@ -31,7 +29,7 @@ $result = []
 counter_ok = 0
 counter_error = 0
 
-# turn off GoodData logging
+# turn off logging for clear output
 GoodData.logging_off
 
 # connect to GoodData
@@ -40,42 +38,43 @@ client = GoodData.connect(login: username, password: password, server: server)
 # connect to development project
 development_project = client.projects(development_project)
 
-# select metrics include and exclude tags
-development_project_metrics = development_project.metrics.select { |metric| (tags_included.empty? || !(metric.tag_set & tags_included).empty?) && (metric.tag_set & tags_excluded).empty? }.sort_by(&:title)
+development_project.metrics.peach do |metric|
 
-# iterate throught every metric
-  development_project_metrics.peach do |metric|
-    if special_tags.any? { |s| metric.title.include?(s) }
-      then
-      begin
-      if  metric.execute >= 0 && metric.execute <= 100 then
-        counter_ok += 1
-      else
-        counter_error += 1
-        output.push(details = {
-        :type => 'ERROR',
-        :url => server + '#s=' + development_project.uri + '|objectPage|' + metric.uri,
-        :api => server + metric.uri,
-        :title => metric.title,
-        :description => 'The result of the metric is out of the range.'
-        })
+  # check included and excluded tags
+  if tags_included.empty? || !(metric.tag_set & tags_included).empty?
+    if (metric.tag_set & tags_excluded).empty?
+
+      if title_illegal_substrings.any? { |title_illegal_substring| metric.title.include? title_illegal_substring }
+
+        begin
+          metric_result = metric.execute
+          if metric_result >= 0 && metric_result <= 1
+            counter_ok += 1
+          else
+            output.push(details = {
+                :type => 'ERROR',
+                :url => server + '#s=' + development_project.uri + '|objectPage|' + metric.uri,
+                :api => server + metric.uri,
+                :title => metric.title,
+                :description => 'The result ' + metric_result + ' of the metric is out of the range.'
+            })
+            counter_error += 1
+          end
+        rescue
+          output.push(details = {
+              :type => 'ERROR',
+              :url => server + '#s=' + development_project.uri + '|objectPage|' + metric.uri,
+              :api => server + metric.uri,
+              :title => metric.title,
+              :description => 'The metric is uncomputable.'
+          })
+          counter_error += 1
+        end
       end
-      rescue
-        counter_error += 1
-        output.push(details = {
-        :type => 'ERROR',
-        :url => server + '#s=' + development_project.uri + '|objectPage|' + metric.uri,
-        :api => server + metric.uri,
-        :title => metric.title,
-        :description => 'The metric is uncomputable.'
-        })
-      end
-end
+    end
   end
+end
 
- $result.push({:section => 'Percentage based metrics check.', :OK => counter_ok, :ERROR => counter_error, :output =>  output})
-
-# result as json_file
+$result.push({:section => 'Percentage based metrics check.', :OK => counter_ok, :ERROR => counter_error, :output => output})
 puts $result.to_json
-
-GoodData.disconnect
+client.disconnect
